db87a22bee5e181a4baa2316ef03dea21178ee5b
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-11-10 23:04:07 +0530

    add

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,81 +13,81 @@ import {Link} from "react-router-dom";
 const Login = ()=>{
     return(
         <>
-                <div class="my_main_wrap">
-        <div class="my_main_inside">
-            <div class="my_login_wrap">
-                <div class="my_login_inside grid justify_sp_between">
-                    <div class="my_login_left_wrap">
-                        <div class="my_login_left__inside flex justify_sp_between flex_column">
+                <div className="my_main_wrap">
+        <div className="my_main_inside">
+            <div className="my_login_wrap">
+                <div className="my_login_inside grid justify_sp_between">
+                    <div className="my_login_left_wrap">
+                        <div className="my_login_left__inside flex justify_sp_between flex_column">
                             
-                            <div class="my_login_left_blob">
+                            <div className="my_login_left_blob">
                                 <img src={blob}/>
-                                <p class="my_login_left_blob_text">Welcome</p>
+                                <p className="my_login_left_blob_text">Welcome</p>
                             </div>
-                            <div class="my_login_left_blob_1">
+                            <div className="my_login_left_blob_1">
                                 <img src={blob_1}/>
                             </div>
-                            <div class="my_login_left_blob_2">
+                            <div className="my_login_left_blob_2">
                                 <img src={blob_2}/>
                             </div>
 
                         </div>
 
                     </div>
-                    <div class="my_login_right_wrap">
-                        <div class="my_login_right__inside flex justify_sp_between flex_column align_center">
-                            <div class="my_login_right_top_text">
+                    <div className="my_login_right_wrap">
+                        <div className="my_login_right__inside flex justify_sp_between flex_column align_center">
+                            <div className="my_login_right_top_text">
                                 <p>My Market</p>
 
                             </div>
-                            <div class="my_login_right_mid_wrap">
-                                <div class="my_login_right_mid_inside">
-                                    <div class="my_login_right_welcome text_center">
+                            <div className="my_login_right_mid_wrap">
+                                <div className="my_login_right_mid_inside">
+                                    <div className="my_login_right_welcome text_center">
                                         <p>Sign In</p>
                                     </div>
-                                    <div class="my_login_right_login_text text_center">
+                                    <div className="my_login_right_login_text text_center">
                                         <p>Enter your email and password to access your account</p>
                                     </div>
                                     <div>
                                         <label>Email / Phone No.</label>
                                     </div>
-                                    <div class="my_login_right_email_text">
+                                    <div className="my_login_right_email_text">
                                         <input type="email" placeholder="Enter your email / phone no."/>
-                                        <span class="my_login_right_email_icon">
+                                        <span className="my_login_right_email_icon">
                                             <img src={email}/>
                                         </span>
                                     </div>
                                     <div>
                                         <label>Password</label>
                                     </div>
-                                    <div class="my_login_right_password_text">
+                                    <div className="my_login_right_password_text">
                                         <input type="password" placeholder="Enter your password"/>
-                                        <span class="my_login_right_password_icon">
+                                        <span className="my_login_right_password_icon">
                                             <img src={password}/>
                                         </span>
-                                        <span class="my_login_right_show_hide_icon">
+                                        <span className="my_login_right_show_hide_icon">
                                             <img src={eye_open}/>
                                             <img src={eye_close} style={{"display": "none"}}/>
                                         </span>
                                     </div>
-                                    <div class="my_login_right_remember_forgot flex justify_sp_between">
-                                        <div class="my_login_right_remember_me">
-                                            <input type="checkbox" id="remember_me" /><label for="remember_me">Remember me</label>
+                                    <div className="my_login_right_remember_forgot flex justify_sp_between">
+                                        <div className="my_login_right_remember_me">
+                                            <input type="checkbox" id="remember_me" /><label htmlFor="remember_me">Remember me</label>
                                         </div>
                                         <div>
                                             <label><Link to="/forgotpass">Forgot Password</Link></label>
                                         </div>
                                     </div>
-                                    <div class="sign_in_button">
+                                    <div className="sign_in_button">
                                         <a href="#">Login</a>
                                     </div>
-                                    <div class="login_separator">
+                                    <div className="login_separator">
                                         <hr/>
                                         <span>
                                             <p>OR</p>
                                         </span>
                                     </div>
-                                    <div class="google_sign_in_button">
+                                    <div className="google_sign_in_button">
                                         <a href="#">Sign In with Google</a>
                                     </div>
                                     
@@ -96,8 +96,8 @@ const Login = ()=>{
                                 </div>
 
                             </div>
-                            <div class="my_login_right_bottom_text">
-                                <p class="my_login_right_bottom_text_unit">Dont have an account? <span><Link to="/register">Sign Up</Link></span></p>
+                            <div className="my_login_right_bottom_text">
+                                <p className="my_login_right_bottom_text_unit">Dont have an account? <span><Link to="/register">Sign Up</Link></span></p>
                             </div>
 
                         </div>
